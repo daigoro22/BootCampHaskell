176b9d4ee0210dcf2b57a771430112e105fbf9a0
daigoro <daigoro.abe@systemi.co.jp>
2022-05-13 17:25:38 +0900

    引数を修正

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Lib
+import UpperCaseOnlyHead
 
 main :: IO ()
-main = someFunc
+main = showUpper
--- app/UpperCaseOnlyHead.hs
+++ app/UpperCaseOnlyHead.hs
@@ -8,4 +8,4 @@ toUpperCaseOnlyHead::String->String
 toUpperCaseOnlyHead s = concatMap(\x -> toUpper (head x) :tail x ++ " ")(words s)
 
 showUpper::IO ()
-showUpper = putStrLn $ toUpperCaseOnlyHead ("To Be, Or Not To Be: That Is The Question.")
+showUpper = putStrLn $ toUpperCaseOnlyHead ("To be, or not to be: that is the question.")
